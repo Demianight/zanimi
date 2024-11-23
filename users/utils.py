410fab905ff2ba7d7ae0a4cc1f95b11c25e3a8bf
Demianight <komarovdemian@yandex.ru>
2024-11-24 00:21:36 +0300

    feat: added utils to work with user information

---
+++ users/utils.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timedelta
+from fastapi import HTTPException
+from passlib.context import CryptContext
+import jwt
+
+from settings import settings
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def create_jwt_token(
+    data: dict,
+) -> str:
+    to_encode = data.copy()
+    to_encode.update({"exp": datetime.utcnow() + timedelta(days=30)})
+    encoded_jwt = jwt.encode(
+        to_encode,
+        settings.token.secret_key,
+        algorithm="HS256",
+    )
+    return encoded_jwt
+
+
+def decode_access_token(token: str) -> dict:
+    try:
+        decoded_jwt = jwt.decode(
+            token,
+            settings.token.secret_key,
+            algorithms=["HS256"],
+        )
+        return decoded_jwt
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token has expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Decoded invalid token")
