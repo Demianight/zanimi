a0d4d844d18805155a47aeefc513024a528347c6
Demianight <komarovdemian@yandex.ru>
2024-11-24 00:22:07 +0300

    feat: added hashed passwords to users and get by username

--- users/crud.py
+++ users/crud.py
@@ -2,12 +2,15 @@ from sqlalchemy.exc import NoResultFound
 from sqlmodel import Session, select
 
 from users.models import User
+from users.utils import hash_password
 
 
 def create_user(
     session: Session, username: str, password: str, bars_id: int
 ) -> User:
-    new_user = User(username=username, password=password, bars_id=bars_id)
+    new_user = User(
+        username=username, password=hash_password(password), bars_id=bars_id
+    )
     session.add(new_user)
     session.commit()
     session.refresh(new_user)
@@ -17,6 +20,16 @@ def create_user(
 # Read (get) a single user by ID
 def get_user_by_id(session: Session, user_id: int) -> User | None:
     statement = select(User).where(User.id == user_id)
+    try:
+        result = session.exec(statement).one()
+        return result
+    except NoResultFound:
+        return None
+
+
+# Read (get) a single user by username
+def get_user_by_username(session: Session, username: str) -> User | None:
+    statement = select(User).where(User.username == username)
     try:
         result = session.exec(statement).one()
         return result
@@ -44,7 +57,7 @@ def update_user(
     if username is not None:
         user.username = username
     if password is not None:
-        user.password = password
+        user.password = hash_password(password)
     if bars_id is not None:
         user.bars_id = bars_id
 
