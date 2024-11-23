d49f83a457e572db57ad92781ec98ce7bc4f4a9e
Demianight <komarovdemian@yandex.ru>
2024-11-24 00:20:53 +0300

    feat: added settings

---
+++ settings.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class TokenSettings(BaseModel):
+    secret_key: str
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_nested_delimiter="__",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+    )
+
+    token: TokenSettings
+
+    debug: bool
+    base_dir: Path = Path(__file__).resolve().parent
+
+
+settings = Settings()  # type: ignore
