47f2448ade2fd7fb626317966c0aade21f037bf8
Zer0-808 <ahevlo888@gmail.com>
2024-12-13 18:30:13 +0300

    Недопопытка 3

--- halls/routers.py
+++ halls/routers.py
@@ -4,10 +4,10 @@ from sqlmodel import Session
 from common.dependencies import get_db
 from halls import crud
 
-from .models import HallPublic
+from .models import HallPublic, HallPublicWithBlocks
 
 router = APIRouter(
-    prefix="/hall",
+    prefix="/halls",
     tags=["halls"],
 )
 
@@ -18,7 +18,7 @@ def get_halls(db: Session = Depends(get_db)) -> list[HallPublic]:
 
 
 @router.get("/{hall_id}")
-def get_hall(hall_id: int, db: Session = Depends(get_db)) -> HallPublic:
+def get_hall(hall_id: int, db: Session = Depends(get_db)) -> HallPublicWithBlocks:
     if hall := crud.get_hall_by_id(db, hall_id):
         return hall
     raise HTTPException(status_code=404, detail="Hall not found")
