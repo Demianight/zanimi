3c31f1d3be24d8d5f3660ae299a666d62b0b5051
Demianight <komarovdemian@yandex.ru>
2024-12-15 14:41:31 +0300

    чуть чуть докрутил пагинацию, добавил создание тестовых залов, добавил предмет занятия

--- database.py
+++ database.py
@@ -1,4 +1,8 @@
-from sqlmodel import SQLModel, create_engine
+from datetime import datetime, timedelta
+
+from sqlmodel import Session, SQLModel, create_engine
+
+from common.utils import create_hall
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -12,3 +16,32 @@ def create_db_and_tables():
     from users.models import User  # noqa
 
     SQLModel.metadata.create_all(engine)
+
+
+if __name__ == "__main__":
+    create_db_and_tables()
+
+    with Session(engine) as session:
+        next_lesson = timedelta(minutes=95 + 15)
+
+        date = datetime(2024, 12, 17, 9, 20)
+        create_hall(
+            session=session,
+            name="Б-201",
+            subject="Физика",
+            schedule=date,
+        )
+        date = date + next_lesson
+        create_hall(
+            session=session,
+            name="В-301",
+            subject="Программирование",
+            schedule=date,
+        )
+        date = date + 2 * next_lesson
+        create_hall(
+            session=session,
+            name="Ж-401",
+            subject="История",
+            schedule=date,
+        )
--- halls/routers.py
+++ halls/routers.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Query, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlmodel import Session
 
 from common.dependencies import get_db
@@ -16,17 +16,19 @@ router = APIRouter(
 def get_halls(
     limit: int = Query(default=10, ge=1, le=100),
     offset: int = Query(default=0, ge=0),
-    relevant_only: bool = True,
+    relevant_only: bool = False,
     minutes: int = Query(default=15, ge=0),
     db: Session = Depends(get_db),
 ) -> list[HallPublic]:
-    if hall := crud.get_all_paginated_halls(db, offset, limit, relevant_only, minutes):
-        return hall
-    raise HTTPException(status_code=404, detail="No items found.")
+    return crud.get_all_paginated_halls(
+        db, offset, limit, relevant_only, minutes
+    )
 
 
 @router.get("/{hall_id}")
-def get_hall(hall_id: int, db: Session = Depends(get_db)) -> HallPublicWithBlocks:
+def get_hall(
+    hall_id: int, db: Session = Depends(get_db)
+) -> HallPublicWithBlocks:
     if hall := crud.get_hall_by_id(db, hall_id):
         return hall
     raise HTTPException(status_code=404, detail="Hall not found")
